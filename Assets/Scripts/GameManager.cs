06846fd4c4d87f53f6d67b3dd151911d5ff42ce1
Aiden <aiden.gimpel@ontariotechu.net>
2021-10-05 13:08:17 -0400

    Added UI for Coin Counter

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] Text coinText;
+
     // Update is called once per frame
     void Update()
     {
+        coinText.text = "Coins: " + FindObjectOfType<PlayerController>().GetCoinCount() + " / 10";
+
         if  (FindObjectOfType<PlayerController>().GetCoinCount() > 9)
         {
             SceneManager.LoadScene("End");
