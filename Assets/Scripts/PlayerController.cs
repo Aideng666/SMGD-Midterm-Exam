fb58e50356e09f37c9c5024b7b94773d3a20bec6
Aiden <aiden.gimpel@ontariotechu.net>
2021-10-06 16:23:03 -0400

    Removed Extra Assets

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,12 +17,6 @@ public class PlayerController : MonoBehaviour
 
     bool isWalking;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
